7b7b0ba5c0428c2aaf85d5e7d94eff239420efd3
StevenC-623 <130469793+StevenC-623@users.noreply.github.com>
2024-02-04 00:06:12 -0500

    Animation + linked up and box is being shot

--- Assets/Scrpt/Shoot.cs
+++ Assets/Scrpt/Shoot.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Security;
 using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
     public GameObject projectile;
+    public GameObject gunner;
     public Transform point;
     public int speed;
+
 
     private int count=0;
 
@@ -14,21 +17,26 @@ public class Shoot : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0) && count < 2)
         {
+            gunner.GetComponent<Animator>().Play("GunnerShoot");
             GameObject bul = (GameObject)Instantiate(projectile, point.transform.position, Quaternion.identity);
-            bul.gameObject.GetComponent<Rigidbody>().velocity = Camera.main.transform.forward * speed;
+            bul.gameObject.GetComponent<Rigidbody>().velocity = Camera.main.transform.right * speed;
+          
             count++;
         }
         else if (Input.GetMouseButtonDown(0) && count == 2)
         {
+            gunner.GetComponent<Animator>().Play("GunnerShoot");
             GameObject bul = (GameObject)Instantiate(projectile, point.transform.position, Quaternion.identity);
             bul.gameObject.GetComponent<Collision>().setBlastRadius(2.5f);
             bul.gameObject.GetComponent<Collision>().setExplosionPower(300);
-            bul.gameObject.GetComponent<Rigidbody>().velocity = Camera.main.transform.forward * speed;
+            bul.gameObject.GetComponent<Rigidbody>().velocity = Camera.main.transform.right * speed;
             count++;
         }
         else
         {
             //play empty clicking sound
         }
+
     }
 }
+
